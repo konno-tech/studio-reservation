9dc9b4658c894e448d23ceb28d8d54edcab7c263
konno-tech <kontc723@gmail.com>
2020-11-15 22:32:44 +0900

    【admin】編集機能実装

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,4 +1,5 @@
 class StudiosController < ApplicationController
+  before_action :set_studio, only: [:edit, :update]
 
   def new
     if admin_signed_in?
@@ -21,7 +22,28 @@ class StudiosController < ApplicationController
     end
   end
 
+  def edit
+    if admin_signed_in?
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if admin_signed_in?
+      if @studio.update(studio_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
+  end
+
   private
+
+  def set_studio
+    @studio = Studio.find(params[:id])
+  end
 
   def studio_params
     params.require(:studio).permit(:name, :price).merge(admin_id: current_admin.id)
