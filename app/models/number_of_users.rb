c05154984de15f2b9a3098232b044435af31868b
konno-tech <kontc723@gmail.com>
2020-11-16 21:50:09 +0900

    利用者数のactivehashの作成

---
+++ app/models/number_of_users.rb
@@ -0,0 +1,15 @@
+class NumberOfUsers < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '1人' },
+    { id: 2, name: '2人' },
+    { id: 3, name: '3人' },
+    { id: 4, name: '4人' },
+    { id: 5, name: '5人' },
+    { id: 6, name: '6人' },
+    { id: 7, name: '7人' },
+    { id: 8, name: '8人' },
+    { id: 9, name: '9人' },
+    { id: 10, name: '10人' }
+  ]
+end
