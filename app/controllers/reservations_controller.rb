3ea707f2055b9e05ef433e8e521339d1ddefd622
konno-tech <kontc723@gmail.com>
2020-11-25 16:29:35 +0900

    Issue6（予約一覧・詳細画面の終了時間修正）

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,9 +2,13 @@ class ReservationsController < ApplicationController
   def index
     @studios = Studio.includes(:admin)
     @reservations = Reservation.all.order(time_from_id: 'ASC')
+    @reservations.each do |reservation|
+      reservation.time_to_id += 1
+    end
   end
 
   def show
     @reservation = Reservation.find(params[:id])
+    @reservation.time_to_id += 1
   end
 end
